6c215df400316d02ee225f76b1554e550868236d
hemarina <hemarina@microsoft.com>
2025-01-07 21:06:45 -0800

    use install script

--- src/main.ts
+++ src/main.ts
@@ -9,75 +9,29 @@ async function run(): Promise<void> {
     // get version number from input
     const version = core.getInput('version')
 
-    // get architecture and os
-    const architecture = process.arch
+    // get os
     const os = process.platform
 
-    // map for different platform and arch
-    const extensionMap = {
-      linux: '.tar.gz',
-      darwin: '.zip',
-      win32: '.zip'
+    let windowsInstallScript = `powershell -ex AllSigned -c "Invoke-RestMethod 'https://aka.ms/install-azd.ps1' | Invoke-Expression"`;
+    let linuxOrMacOSInstallScript = `curl -fsSL https://aka.ms/install-azd.sh | bash`;
+    if (version){
+      windowsInstallScript = `powershell -ex AllSigned -c "Invoke-RestMethod 'https://aka.ms/install-azd.ps1' -OutFile 'install-azd.ps1'; ./install-azd.ps1 -Version '${version}'"`;
+      linuxOrMacOSInstallScript = `curl -fsSL https://aka.ms/install-azd.sh | bash -s -- --version ${version}`;
+    }
+    
+    if (os === 'win32') {
+      cp.execSync(windowsInstallScript);
+    } else  {
+      cp.execSync(linuxOrMacOSInstallScript);
     }
 
-    const exeMap = {
-      linux: '',
-      darwin: '',
-      win32: '.exe'
-    }
-
-    const arm64Map = {
-      x64: 'amd64',
-      arm64: 'arm64-beta'
-    }
-
-    const platformMap = {
-      linux: 'linux',
-      darwin: 'darwin',
-      win32: 'windows'
-    }
-
-    // get install url
-    const installArray = installUrlForOS(
-      os,
-      architecture,
-      platformMap,
-      arm64Map,
-      extensionMap,
-      exeMap
-    )
-
-    const url = `https://azd-release-gfgac2cmf7b8cuay.b02.azurefd.net/azd/standalone/release/${version}/${installArray[0]}`
 
     core.notice(`The Azure Developer CLI collects usage data and sends that usage data to Microsoft in order to help us improve your experience.
 You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use.
 
 Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-dev#data-collection`)
 
-    core.info(`Installing azd from ${url}`)
-
-    const file = await toolCache.downloadTool(url)
-    let extracted
-    if (os !== 'linux') {
-      extracted = await toolCache.extractZip(file)
-    } else {
-      extracted = await toolCache.extractTar(file)
-    }
-
-    if (os !== 'win32') {
-      fs.symlinkSync(
-        path.join(extracted, installArray[1]),
-        path.join(extracted, 'azd')
-      )
-    } else {
-      fs.symlinkSync(
-        path.join(extracted, installArray[1]),
-        path.join(extracted, 'azd.exe')
-      )
-    }
-
-    core.info(`azd installed to ${extracted}`)
-    core.addPath(extracted)
+    core.info(`Installing azd version ${version} on ${os}`)
 
     // Run `azd version` so we get the version that was installed written to the log.
     core.info(cp.execSync('azd version').toString())
@@ -86,29 +40,6 @@ Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-de
       core.setFailed(error.message)
     }
   }
-}
-
-function installUrlForOS(
-  os: string,
-  architecture: string,
-  platformMap: Record<string, string>,
-  archMap: Record<string, string>,
-  extensionMap: Record<string, string>,
-  exeMap: Record<string, string>
-): [string, string] {
-  const platformPart = `${platformMap[os]}`
-  const archPart = `${archMap[architecture]}`
-
-  if (platformPart === `undefined` || archPart === `undefined`) {
-    throw new Error(
-      `Unsupported platform and architecture: ${architecture} ${os}`
-    )
-  }
-
-  const installUrl = `azd-${platformPart}-${archPart}${extensionMap[os]}`
-  const installUrlForRename = `azd-${platformPart}-${archPart}${exeMap[os]}`
-
-  return [installUrl, installUrlForRename]
 }
 
 run()
