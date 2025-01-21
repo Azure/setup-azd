1132d727d46287d6128ef5ef9750fa6425949862
hemarina <104857065+hemarina@users.noreply.github.com>
2025-01-21 15:11:24 -0800

    switch to use install script (#236)
    * use install script
    
    * npm run package
    
    * add it to user level instead of machine level
    
    * remove toolCache unuse
    
    * fix npm run lint error
    
    * Update src/main.ts
    
    Co-authored-by: Victor Vazquez <victor.vazquez@microsoft.com>
    
    * Update src/main.ts
    
    Co-authored-by: Victor Vazquez <victor.vazquez@microsoft.com>
    
    * npm run all with latest changes
    
    * remove sudo from curl
    
    * add return after core set failed and npm run all
    
    * remove second check on localAppDataPath
    
    * apply suggestion from review
    
    * npm run all and make sure localappdata check only in windows
    
    * test with -Verbose
    
    * use -Verbose:$true in windows
    
    * testing
    
    * Print out verbose output
    
    * add azd version back
    
    * run azd version to verify installation in windows only
    
    ---------
    
    Co-authored-by: Victor Vazquez <victor.vazquez@microsoft.com>

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,57 @@
 import * as fs from 'fs'
 import * as cp from 'child_process'
 import * as core from '@actions/core'
-import * as toolCache from '@actions/tool-cache'
-import path from 'path'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
+    // get os
+    const os = process.platform
+    const localAppData = process.env.LocalAppData
+    const githubPath = process.env.GITHUB_PATH
+    if (os === 'win32' && !localAppData) {
+      core.setFailed('LocalAppData environment variable is not defined.')
+      return
+    }
+    if (!githubPath) {
+      core.setFailed('GITHUB_PATH environment variable is not defined.')
+      return
+    }
+
     // get version number from input
     const version = core.getInput('version')
+    const windowsInstallScript = `powershell -c "$scriptPath = \\"$($env:TEMP)\\install-azd.ps1\\"; Invoke-RestMethod 'https://aka.ms/install-azd.ps1' -OutFile $scriptPath; . $scriptPath -Version '${version}' -Verbose:$true; Remove-Item $scriptPath"`
+    const linuxOrMacOSInstallScript = `curl -fsSL https://aka.ms/install-azd.sh | sudo bash -s -- --version ${version} --verbose`
 
-    // get architecture and os
-    const architecture = process.arch
-    const os = process.platform
+    core.info(`Installing azd version ${version} on ${os}.`)
 
-    // map for different platform and arch
-    const extensionMap = {
-      linux: '.tar.gz',
-      darwin: '.zip',
-      win32: '.zip'
-    }
+    if (os === 'win32' && localAppData) {
+      core.info(cp.execSync(windowsInstallScript).toString())
 
-    const exeMap = {
-      linux: '',
-      darwin: '',
-      win32: '.exe'
-    }
-
-    const arm64Map = {
-      x64: 'amd64',
-      arm64: 'arm64-beta'
-    }
-
-    const platformMap = {
-      linux: 'linux',
-      darwin: 'darwin',
-      win32: 'windows'
-    }
-
-    // get install url
-    const installArray = installUrlForOS(
-      os,
-      architecture,
-      platformMap,
-      arm64Map,
-      extensionMap,
-      exeMap
-    )
-
-    const url = `https://azd-release-gfgac2cmf7b8cuay.b02.azurefd.net/azd/standalone/release/${version}/${installArray[0]}`
-
-    core.notice(`The Azure Developer CLI collects usage data and sends that usage data to Microsoft in order to help us improve your experience.
-You can opt-out of telemetry by setting the AZURE_DEV_COLLECT_TELEMETRY environment variable to 'no' in the shell you use.
-
-Read more about Azure Developer CLI telemetry: https://github.com/Azure/azure-dev#data-collection`)
-
-    core.info(`Installing azd from ${url}`)
-
-    const file = await toolCache.downloadTool(url)
-    let extracted
-    if (os !== 'linux') {
-      extracted = await toolCache.extractZip(file)
+      // Add azd to PATH
+      const azdPath = path.join(localAppData, 'Programs', 'Azure Dev CLI')
+      fs.appendFileSync(githubPath, `${azdPath}${path.delimiter}`)
     } else {
-      extracted = await toolCache.extractTar(file)
+      core.info(cp.execSync(linuxOrMacOSInstallScript).toString())
     }
 
-    if (os !== 'win32') {
-      fs.symlinkSync(
-        path.join(extracted, installArray[1]),
-        path.join(extracted, 'azd')
+    // Run `azd version` to make sure if azd installation failed, it returns error on windows
+    // if installation is not successful, linux and macos fail when running install scripts
+    if (os === 'win32' && localAppData) {
+      const azdExePath = path.join(
+        localAppData,
+        'Programs',
+        'Azure Dev CLI',
+        'azd.exe'
       )
-    } else {
-      fs.symlinkSync(
-        path.join(extracted, installArray[1]),
-        path.join(extracted, 'azd.exe')
-      )
+      const azdVersion = `"${azdExePath}" version`
+      cp.execSync(azdVersion)
     }
-
-    core.info(`azd installed to ${extracted}`)
-    core.addPath(extracted)
-
-    // Run `azd version` so we get the version that was installed written to the log.
-    core.info(cp.execSync('azd version').toString())
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
     }
   }
-}
-
-function installUrlForOS(
-  os: string,
-  architecture: string,
-  platformMap: Record<string, string>,
-  archMap: Record<string, string>,
-  extensionMap: Record<string, string>,
-  exeMap: Record<string, string>
-): [string, string] {
-  const platformPart = `${platformMap[os]}`
-  const archPart = `${archMap[architecture]}`
-
-  if (platformPart === `undefined` || archPart === `undefined`) {
-    throw new Error(
-      `Unsupported platform and architecture: ${architecture} ${os}`
-    )
-  }
-
-  const installUrl = `azd-${platformPart}-${archPart}${extensionMap[os]}`
-  const installUrlForRename = `azd-${platformPart}-${archPart}${exeMap[os]}`
-
-  return [installUrl, installUrlForRename]
 }
 
 run()
